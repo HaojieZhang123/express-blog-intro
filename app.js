416d243a8aaf5e1244cac900e331c6b1b8abeb69
HaojieZhnag123 <haojiezhnag123@gmail.com>
2025-04-16 17:22:32 +0200

    [UPD] additional route to /bacheca

--- app.js
+++ app.js
@@ -10,31 +10,31 @@ const blog = [
     {
         title: 'titolo',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
-        image: '',
+        image: 'public/img/1.jpg',
         tags: ['tag1', 'tag2', 'tag3']
     },
     {
         title: 'titolo',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
-        image: 'adb',
+        image: 'public/img/2.jpg',
         tags: ['tag1', 'tag2', 'tag3']
     },
     {
         title: 'titolo',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
-        image: 'adb',
+        image: 'public/img/3.jpg',
         tags: ['tag1', 'tag2', 'tag3']
     },
     {
         title: 'titolo',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
-        image: 'adb',
+        image: 'public/img/4.jpg',
         tags: ['tag1', 'tag2', 'tag3']
     },
     {
         title: 'titolo',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris.',
-        image: 'adb',
+        image: 'public/img/5.jpg',
         tags: ['tag1', 'tag2', 'tag3']
     }
 ];
@@ -45,6 +45,11 @@ app.use(express.static('public'));
 // base root
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
+});
+
+// route for images '/bacheca'
+app.get('/bacheca', (req, res) => {
+    res.json(blog);
 });
 
 // leave server to listen to any request coming into the specified port
